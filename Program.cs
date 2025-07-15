1e4a2ca993e06fc7ce9f574f318e4fd787010e54
Damodar Nayak <31372466+WoollyAttorney@users.noreply.github.com>
2025-07-16 02:18:48 +0800

    add program to run edge detection

--- Program.cs
+++ Program.cs
@@ -1,2 +1,72 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Edge_Detection.Core;
+using SkiaSharp;
+
+namespace Edge_Detectionls
+{
+    public class EdgeDetectorProgram
+    {
+
+        public static void Main(string[] args)
+        {
+            string? input;
+
+            Console.WriteLine("Enter path/to/image: ");
+            input = Console.ReadLine();
+            if (string.IsNullOrEmpty(input))
+            {
+                Console.WriteLine("No input provided. Exiting program.");
+                return;
+            }
+            string imagePath = input;
+
+            Console.WriteLine("Enter operator name [Sobel, Prewitt]: ");
+            input = Console.ReadLine();
+            if (string.IsNullOrEmpty(input))
+            {
+                Console.WriteLine("No input provided. Exiting program.");
+                return;
+            }
+            string operatorString = input;
+
+            Console.WriteLine("Enter output/path/to/image ");
+            input = Console.ReadLine();
+            string output;
+            if (string.IsNullOrEmpty(input))
+            {
+                Console.WriteLine("No input provided. Storing the image as output.png");
+                output = "output.png";
+            }
+            else
+                output = input;
+
+            DetectionOperator detectionOperator;
+            if (operatorString.Equals("Sobel", StringComparison.OrdinalIgnoreCase))
+            {
+                detectionOperator = DetectionOperator.Sobel;
+            }
+            else if (operatorString.Equals("Prewitt", StringComparison.OrdinalIgnoreCase))
+            {
+                detectionOperator = DetectionOperator.Prewitt;
+            }
+            else
+            {
+                Console.WriteLine("Invalid operator name. Exiting program.");
+                return;
+            }
+
+            using var rawData = File.OpenRead(imagePath);
+            using var inputStream = new SKManagedStream(rawData);
+            using var image = SKBitmap.Decode(inputStream);
+            if (image == null)
+            {
+                Console.WriteLine("Error opening image file");
+                return;
+            }
+            SKBitmap outputImage = Detector.DetectEdges(image, detectionOperator);
+            using (var outputStream = File.OpenWrite(output))
+            {
+                outputImage.Encode(SKEncodedImageFormat.Png, 100).SaveTo(outputStream);
+            }
+        }
+    }
+}
